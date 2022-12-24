ff9364a879068ec0f54db311b44d614aba72dd75
Daniel <daniel.cares19@gmail.com>
2022-12-24 02:57:05 -0300

    Merge remote-tracking branch 'origin/DEV' into DEV

--- app/src/main/java/com/example/pruebafinal/MainActivity5.java
+++ app/src/main/java/com/example/pruebafinal/MainActivity5.java
@@ -3,12 +3,35 @@ package com.example.pruebafinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    private Button btnVolverEventos;
+
+
+    //References
+    private void referencias (){
+        btnVolverEventos = findViewById(R.id.btnVolverEventos);
+    }
+
+    private void eventos(){
+        btnVolverEventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        referencias();
+        eventos();
     }
+
+
 }
